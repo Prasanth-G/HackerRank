9f304d07ea372855fc703cdb844f6b4d78af0e5b
Prasanth-G <prasanthganesamoorthy@outlook.com>
2017-06-03 11:06:27 +0530

    Warmup- matching/excluding char

---
+++ Character_class.py
@@ -0,0 +1,8 @@
+#1.Matching Specific Characters
+Regex_Pattern = r'^[123][012][xs0][30Aa][xsu][\.,]$'
+
+#2.Excluding Specific Characters
+Regex_Pattern = r'^[^\d][^aeiou][^bcDF][^\s][^AEIOU][^\.,]$'
+
+#3.Matching Character Ranges
+Regex_Pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
